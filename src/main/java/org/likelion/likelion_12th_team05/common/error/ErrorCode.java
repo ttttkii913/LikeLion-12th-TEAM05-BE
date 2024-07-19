f422715552d1439840bb08cf6e50cd63922ed764
ttttkii913 <dbgkdud821@daum.net>
2024-07-20 01:25:51 +0900

    Feat: ApiResponseTemplate 추가

---
+++ src/main/java/org/likelion/likelion_12th_team05/common/error/ErrorCode.java
@@ -0,0 +1,32 @@
+package org.likelion.likelion_12th_team05.common.error;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorCode { // 반복적으로 사용될 Error 상태와 메세지, 코드를 정의
+                        // 시스템에 비정상적인 상황이 발생했을 경우
+    /**
+     * 404 NOT FOUND
+     */
+    CURATIONS_NOT_FOUND_EXCEPTION("해당 큐레이션이 없습니다. NOT_FOUND_404"),
+    LOCATIONS_NOT_FOUND_EXCEPTION("해당 위치가 없습니다. NOT_FOUND_404"),
+    USER_NOT_FOUND_EXCEPTION("해당 사용자가 없습니다. NOT_FOUND_404"),
+    FILE_NOT_FOUND_EXCEPTION("파일을 찾을 수 없습니다."),
+    /**
+     * 500 INTERNAL SERVER ERROR
+     */
+    INTERNAL_SERVER_ERROR("알 수 없는 서버 에러가 발생했습니다. INTERNAL_SERVER_ERROR_500"),
+
+    /**
+     * 400 BAD REQUEST
+     */
+    VALIDATION_ERROR("BAD_REQUEST_400");
+
+    private final String message;
+
+
+}
---
+++ src/main/java/org/likelion/likelion_12th_team05/common/error/SuccessCode.java
@@ -0,0 +1,31 @@
+package org.likelion.likelion_12th_team05.common.error;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessCode {   // Success 상태와 메세지, 코드를 정의
+    /**
+     * 200 OK   (요청 성공
+     */
+    GET_SUCCESS("성공적으로 조회했습니다."),
+    LOCATION_UPDATE_SUCCESS("위치가 성공적으로 수정되었습니다."),
+    CURATION_UPDATE_SUCCESS("큐레이션이 성공적으로 수정되었습니다."),
+    LOCATION_DELETE_SUCCESS("위치가 성공적으로 삭제되었습니다."),
+    CURATION_DELETE_SUCCESS( "큐레이션이 성공적으로 삭제되었습니다."),
+    LIKE_SAVE_SUCCESS("좋아요가 성공적으로 저장되었습니다."),
+    LIKE_DELETE_SUCCESS("좋아요가 성공적으로 삭제되었습니다."),
+    USER_SIGNUP_SUCCESS("회원가입에 성공하였습니다."),
+    USER_LOGIN_SUCCESS("로그인에 성공하였습니다."),
+    /**
+     * 201 CREATED (POST의 결과 상태
+     */
+    LOCATION_SAVE_SUCCESS("위치가 성공적으로 등록되었습니다."),
+    CURATION_SAVE_SUCCESS("큐레이션이 성공적으로 등록되었습니다.");
+
+    private final String message;
+
+}
