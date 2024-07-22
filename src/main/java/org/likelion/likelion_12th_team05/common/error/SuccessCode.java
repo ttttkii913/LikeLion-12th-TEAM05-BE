f09673bb543692283156c609bc4a74d842a9117f
ttttkii913 <dbgkdud821@daum.net>
2024-07-23 00:12:49 +0900

    Refactor: 좋아요 상태 코드 추가

--- src/main/java/org/likelion/likelion_12th_team05/common/error/SuccessCode.java
+++ src/main/java/org/likelion/likelion_12th_team05/common/error/SuccessCode.java
@@ -16,7 +16,6 @@ public enum SuccessCode {   // Success 상태와 메세지, 코드를 정의
     CURATION_UPDATE_SUCCESS("큐레이션이 성공적으로 수정되었습니다."),
     LOCATION_DELETE_SUCCESS("위치가 성공적으로 삭제되었습니다."),
     CURATION_DELETE_SUCCESS( "큐레이션이 성공적으로 삭제되었습니다."),
-    LIKE_SAVE_SUCCESS("좋아요가 성공적으로 저장되었습니다."),
     LIKE_DELETE_SUCCESS("좋아요가 성공적으로 삭제되었습니다."),
     USER_SIGNUP_SUCCESS("회원가입에 성공하였습니다."),
     USER_LOGIN_SUCCESS("로그인에 성공하였습니다."),
@@ -24,7 +23,8 @@ public enum SuccessCode {   // Success 상태와 메세지, 코드를 정의
      * 201 CREATED (POST의 결과 상태
      */
     LOCATION_SAVE_SUCCESS("위치가 성공적으로 등록되었습니다."),
-    CURATION_SAVE_SUCCESS("큐레이션이 성공적으로 등록되었습니다.");
+    CURATION_SAVE_SUCCESS("큐레이션이 성공적으로 등록되었습니다."),
+    LIKE_SAVE_SUCCESS("좋아요가 성공적으로 저장되었습니다.");
 
     private final String message;
 
