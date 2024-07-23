d23e7fc1b2fbd25e000253b91bb61897eb036daf
ttttkii913 <dbgkdud821@daum.net>
2024-07-23 21:18:30 +0900

    Fix: cors 설정 제거

--- src/main/java/org/likelion/likelion_12th_team05/global/auth/config/SecurityConfig.java
+++ src/main/java/org/likelion/likelion_12th_team05/global/auth/config/SecurityConfig.java
@@ -50,20 +50,4 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean
-    public CorsConfigurationSource configurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 }
