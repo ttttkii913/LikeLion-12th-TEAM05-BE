b82fc72b1b4dbd9e25a8fcc2a4c5f94a7b858bb0
ttttkii913 <dbgkdud821@daum.net>
2024-07-24 21:45:39 +0900

    Refactor: 스웨거 설명 추가

--- src/main/java/org/likelion/likelion_12th_team05/location/api/LocationController.java
+++ src/main/java/org/likelion/likelion_12th_team05/location/api/LocationController.java
@@ -41,7 +41,7 @@ public class LocationController {
         return ApiResponseTemplate.successResponse(locationListResDto, SuccessCode.GET_SUCCESS);
     }
 
-    @Operation(summary = "인증된 사용자가 위치 생성", description = "인증된 사용자가 위치를 생성합니다.")
+    @Operation(summary = "인증된 사용자가 위치 생성", description = "인증된 사용자가 위치를 생성(큐레이션 id, 위치 이름, 이미지)합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "응답 생성에 성공하였습니다."),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
@@ -57,7 +57,7 @@ public class LocationController {
         return ApiResponseTemplate.successResponse(locationInfoResDto, SuccessCode.LOCATION_SAVE_SUCCESS);
     }
 
-    @Operation(summary = "인증된 사용자가 위치 수정", description = "인증된 사용자가 위치를 수정합니다.")
+    @Operation(summary = "인증된 사용자가 위치 수정", description = "인증된 사용자가 위치를 수정(위치 이름, 이미지)합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "응답 생성에 성공하였습니다."),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
@@ -73,7 +73,7 @@ public class LocationController {
         return ApiResponseTemplate.successResponse(locationInfoResDto, SuccessCode.LOCATION_UPDATE_SUCCESS);
     }
 
-    @Operation(summary = "인증된 사용자가 위치 삭제.", description = "인증된 사용자가 위치를 삭제합니다.")
+    @Operation(summary = "인증된 사용자가 위치 삭제", description = "인증된 사용자가 위치를 삭제합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "응답 생성에 성공하였습니다."),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
