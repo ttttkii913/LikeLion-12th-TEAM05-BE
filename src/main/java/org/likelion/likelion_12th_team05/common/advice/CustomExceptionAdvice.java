ed4afbbf8a890b615acd854688a5be98ca5229ef
ttttkii913 <dbgkdud821@daum.net>
2024-07-28 05:24:45 +0900

    Feat: validation 핸들러, 어노테이션 추가

--- src/main/java/org/likelion/likelion_12th_team05/common/advice/CustomExceptionAdvice.java
+++ src/main/java/org/likelion/likelion_12th_team05/common/advice/CustomExceptionAdvice.java
@@ -1,21 +1,28 @@
 package org.likelion.likelion_12th_team05.common.advice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.likelion.likelion_12th_team05.common.error.ErrorCode;
 import org.likelion.likelion_12th_team05.common.exception.CustomException;
 import org.likelion.likelion_12th_team05.common.exception.NotFoundException;
 import org.likelion.likelion_12th_team05.config.ApiResponseTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class CustomExceptionAdvice {
-
-    private static final Logger log = LoggerFactory.getLogger(CustomExceptionAdvice.class);
 
     // 엔티티 not found 에러 처리 핸들러
     @ExceptionHandler(NotFoundException.class)
@@ -32,10 +39,37 @@ public class CustomExceptionAdvice {
     }
 
     // 원인 모를 이유의 예외 발생 시
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ApiResponseTemplate handleServerException(final Exception e) {
         log.error("Internal Server Error: {}", e.getMessage(), e);
         return ApiResponseTemplate.errorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * 400 BAD REQUEST validation
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponseTemplate<String> handleValidationExceptions(MethodArgumentNotValidException e) {
+        log.error("Validation error: {}", e.getMessage(), e); // 수정: 로그 추가
+
+        // 에러 메시지 생성
+        Map<String, String> errorMap = new HashMap<>();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        // 응답 생성
+        return ApiResponseTemplate.errorResponse(ErrorCode.VALIDATION_ERROR, convertMapToString(errorMap));
+    }
+
+    private String convertMapToString(Map<String, String> map) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            sb.append(entry.getKey()).append(" : ").append(entry.getValue()).append(", ");
+        }
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 2); // 마지막 쉼표와 띄어쓰기를 제거
+        }
+        return sb.toString();
     }
 }
