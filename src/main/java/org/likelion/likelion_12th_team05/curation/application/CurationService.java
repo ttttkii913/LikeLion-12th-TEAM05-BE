a1cc6db487e86cd31eb7d3419d042d925ee2141e
ttttkii913 <dbgkdud821@daum.net>
2024-07-29 02:15:10 +0900

    Feat: 큐레이션 개수 증가, 감소 추가

--- src/main/java/org/likelion/likelion_12th_team05/curation/application/CurationService.java
+++ src/main/java/org/likelion/likelion_12th_team05/curation/application/CurationService.java
@@ -35,6 +35,8 @@ public class CurationService {
         User user = getUserByEmail(email);
 
         Curation curation = curationSaveReqDto.toEntity(user);
+        // 큐레이션 생성 시 사용자마다 큐레이션 개수가 하나 증가하도록 -> 랜딩 페이지에서 큐레이션 가장 많이 작성한 큐레이터 조회하기 위함 -> 삭제 시에도 반영
+        user.setCurationCount(user.getCurationCount() + 1);
         curationRepository.save(curation);
 
         return CurationInfoResDto.from(curation);
@@ -67,12 +69,15 @@ public class CurationService {
 
         // 큐레이션 작성자 이메일과 일치하는지 확인 - 삭제 권한 확인
         String email = principal.getName();
+        User user = getUserByEmail(email);
         String CurationUserEmail = curation.getUser().getEmail();
 
         if (!email.equals(CurationUserEmail)) {
             throw new NotFoundException(ErrorCode.NO_AUTHORIZATION_EXCEPTION
                     , ErrorCode.NO_AUTHORIZATION_EXCEPTION.getMessage());
         }
+        // 큐레이션 삭제 시에 사용자의 큐레이션 개수가 하나 줄어들도록
+        user.setCurationCount(user.getCurationCount() - 1);
         curationRepository.delete(curation);
     }
 
