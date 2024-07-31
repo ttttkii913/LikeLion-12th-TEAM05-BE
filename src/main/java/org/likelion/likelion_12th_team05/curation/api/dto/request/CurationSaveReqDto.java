9179399fe18ee67f1c0adefc8b15aa7ddf9e3999
ttttkii913 <dbgkdud821@daum.net>
2024-07-31 21:28:27 +0900

    Feat: 큐레이션에 validation 추가

--- src/main/java/org/likelion/likelion_12th_team05/curation/api/dto/request/CurationSaveReqDto.java
+++ src/main/java/org/likelion/likelion_12th_team05/curation/api/dto/request/CurationSaveReqDto.java
@@ -1,12 +1,19 @@
 package org.likelion.likelion_12th_team05.curation.api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import org.likelion.likelion_12th_team05.curation.domain.Curation;
 import org.likelion.likelion_12th_team05.user.domain.User;
 
 @Builder
 public record CurationSaveReqDto(
+        @NotBlank(message = "제목은 필수로 입력해야 합니다.")
+        @Size(min = 2, max = 15, message = "2자 이상 15자 이하로 입력해야 합니다.")
         String name,
+
+        @NotBlank(message = "내용은 필수로 입력해야 합니다.")
+        @Size(min = 2, max = 15, message = "2자 이상 15자 이하로 입력해야 합니다.")
         String content
 
 ) {
