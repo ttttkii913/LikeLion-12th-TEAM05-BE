285579bb7f4c278be6882f6198d89bf14e0e5728
ttttkii913 <dbgkdud821@daum.net>
2024-08-01 22:27:04 +0900

    Feat: 위치 저장시 위도, 경도 필드 추가

--- src/main/java/org/likelion/likelion_12th_team05/location/api/dto/request/LocationSaveReqDto.java
+++ src/main/java/org/likelion/likelion_12th_team05/location/api/dto/request/LocationSaveReqDto.java
@@ -7,7 +7,9 @@ import org.likelion.likelion_12th_team05.user.domain.User;
 public record LocationSaveReqDto(
         String name,
         String description,
-        String address
+        String address,
+        Double longitude,
+        Double latitude
 ) {
     public Location toEntity(String locationImage, Curation curation, User user) {
         // 위치 저장 시 이미지를 선택으로 수정/ 이미지가 비어있지 않다면 이미지 저장, 비어있다면 빈 문자열로 locationImage 저장 => location 엔티티에서 locationImage nullable 지워줌
@@ -20,6 +22,8 @@ public record LocationSaveReqDto(
                 .locationImage(image)
                 .curation(curation)
                 .user(user)
+                .longitude(longitude)
+                .latitude(latitude)
                 .build();
     }
 }
