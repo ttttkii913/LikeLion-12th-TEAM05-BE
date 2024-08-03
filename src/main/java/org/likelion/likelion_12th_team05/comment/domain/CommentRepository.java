db96d4975b3a4e631bb4e9b5b4f8a7ecdc71207e
ttttkii913 <dbgkdud821@daum.net>
2024-08-03 18:08:21 +0900

    Feat: 댓글 전체 조회 api 추가

--- src/main/java/org/likelion/likelion_12th_team05/comment/domain/CommentRepository.java
+++ src/main/java/org/likelion/likelion_12th_team05/comment/domain/CommentRepository.java
@@ -2,8 +2,10 @@ package org.likelion.likelion_12th_team05.comment.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findById(Long curationId);
+    List<Comment> findAllByCurationId(Long curationId);
 }
